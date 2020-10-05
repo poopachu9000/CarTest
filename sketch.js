e0ea69b1e79899360cc3372ff38da63a920f78ff
poopachu9000 <71670016+poopachu9000@users.noreply.github.com>
2020-10-05 12:18:09 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -23,15 +23,17 @@ function draw() {
 
   if(wall.x-car.x<(car.width+wall.width)/2){
      car.velocityX = 0;
-  deformation = (0.5*weight*speed*speed)/2;
+  deformation = (0.5*weight*speed*speed)/22500;
 if(deformation < 200){
-car.shapeColor = color(255,0,0);
-}else if(deformation > 100 && deformation<180){
+car.shapeColor = color(0,255,0);
+}
+ if(deformation > 100 && deformation<180){
   car.shapeColor = color(230,230,0);
-} else{
-  car.shapeColor = color(0,255,0);
+} 
+if(deformation>180){
+  car.shapeColor = color(255,0,0);
 }
   }
-  
+  console.log(deformation);
   drawSprites();
 }
